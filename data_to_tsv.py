ba92592bfea47ea257f1aff77647592937ab4479
bcbabrich <39421140+bcbabrich@users.noreply.github.com>
2019-11-18 11:42:42 -0500

    tsv converter now also shuffles and splits into train and test as well

--- data_to_tsv.py
+++ data_to_tsv.py
@@ -1,23 +1,43 @@
 # convert .data files to .tsv files
+# assumes we have raw .data files in local directory
 
-#
+# convert
+# loop over meta data and grab four columns for tsv file
+# put them in a list so we can shuffle and split
 data_dir = 'C:/Users/Chase\'s Laptop/PycharmProjects/Text2Time/dataset_raw/'
 m = open(data_dir + 'meta.csv', 'r')
-t = open('C:/Users/Chase\'s Laptop/PycharmProjects/Text2Time/dataset.tsv', 'w+', encoding='utf8')
-
-# loop over meta data and grab four columns for tsv file
-for row in m :
+t_list = []
+for row in m:
     columns = row.split(',')
-    id = columns[0]
+    ID = columns[0]
     label = columns[1]
     alpha = 'a'
-    raw_f = open(data_dir + id + '.data', 'r', encoding='utf8')
+    raw_f = open(data_dir + ID + '.data', 'r', encoding='utf8')
     text = raw_f.read()
     raw_f.close()
-    s = (id + '\t'
+    s = (ID + '\t'
             + label + '\t'
             + alpha + '\t'
             + text.replace('\n', '') + '\n')
-    t.write(s)
-t.close()
+    t_list.append(s)
 m.close()
+
+# shuffle
+from random import shuffle
+shuffle(t_list)
+
+# split
+train_t = t_list[:int(len(t_list) * 0.8)]
+test_t = t_list[int(len(t_list) * 8):]
+
+# write
+train_f = open('C:/Users/Chase\'s Laptop/PycharmProjects/Text2Time/train.tsv', 'w+', encoding='utf8')
+for line in train_t:
+    train_f.write(line)
+train_f.close()
+
+test_f = open('C:/Users/Chase\'s Laptop/PycharmProjects/Text2Time/test.tsv', 'w+', encoding='utf8')
+for line in test_t:
+    test_f.write(line)
+test_f.close()
+
