ae1ce80746c4a6ed5f6e45e0feb91e83d2ccded7
bcbabrich <39421140+bcbabrich@users.noreply.github.com>
2019-11-19 19:50:48 -0500

    new csv files

--- data_to_tsv.py
+++ data_to_tsv.py
@@ -1,6 +1,8 @@
 # convert .data files to .tsv files
 # assumes we have raw .data files in local directory
 
+#
+
 # convert
 # loop over meta data and grab four columns for tsv file
 # put them in a list so we can shuffle and split
@@ -10,21 +12,18 @@ t_list = []
 for row in m:
     columns = row.split(',')
     ID = columns[0]
-    label = columns[1]
+    label = columns[1][2]
     alpha = 'a'
     raw_f = open(data_dir + ID + '.data', 'r', encoding='utf8')
     text = raw_f.read()
     raw_f.close()
-    s = (ID + ', '
-            + label + ', '
-            + alpha + ', '
-            + text.replace('\n', '').replace(',', '')
-            + '\n ')
+    s = label + ',' + text.replace('\n', '').replace(',', '') + '\n '
     t_list.append(s)
 m.close()
 
 # shuffle
 from random import shuffle
+
 shuffle(t_list)
 
 # split
@@ -32,13 +31,14 @@ train_t = t_list[:int(len(t_list) * 0.8)]
 test_t = t_list[int(len(t_list) * 0.8):]
 6
 # write
-train_f = open('C:/Users/Chase\'s Laptop/PycharmProjects/pytorch-transformers-classification/data/train.csv', 'w+', encoding='utf8')
+train_f = open('C:/Users/Chase\'s Laptop/PycharmProjects/Text2Time/train.csv', 'w+',
+               encoding='utf8')
 for line in train_t:
     train_f.write(line)
 train_f.close()
 
-test_f = open('C:/Users/Chase\'s Laptop/PycharmProjects/pytorch-transformers-classification/data/test.csv', 'w+', encoding='utf8')
+test_f = open('C:/Users/Chase\'s Laptop/PycharmProjects/Text2Time/test.csv', 'w+',
+              encoding='utf8')
 for line in test_t:
     test_f.write(line)
 test_f.close()
-
